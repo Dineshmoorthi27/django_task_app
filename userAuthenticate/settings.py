f44148bead4ea3c10f6e5d7fb7d6431292445615
Dineshmoorthi27 <dineshmoorthi27.gmail.com>
2025-01-29 20:30:05 +0530

    setting updated

--- userAuthenticate/settings.py
+++ userAuthenticate/settings.py
@@ -167,7 +167,6 @@ MEDIA_URL = '/media/'
 MEDIAFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
